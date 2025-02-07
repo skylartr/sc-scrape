498b6fec39ab979f420feb2bbb3e8d927234ee1a
Skylar <79112590+skylartr@users.noreply.github.com>
2025-02-07 01:16:26 -0500

    Update download_scdl.bat

--- download_scdl.bat
+++ download_scdl.bat
@@ -1,36 +1,82 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: Check if artists.txt exists
-if not exist artists.txt (
-    echo artists.txt not found! Create the file and add SoundCloud profile links.
-    pause
-    exit /b
-)
+:: Define paths
+set "artistList=artists.txt"
+set "downloadPath=%CD%\SoundCloudDownloads"
 
-:: Loop through each line in artists.txt
-for /f "tokens=*" %%A in (artists.txt) do (
+:: Ensure the main download folder exists
+if not exist "%downloadPath%" mkdir "%downloadPath%"
+
+:: Read each line from artists.txt
+for /f "tokens=*" %%A in (%artistList%) do (
     set "url=%%A"
-    
-    :: Extract artist name from the URL (last part after the last "/")
-    for %%B in (!url!) do set "artist_name=%%~nxB"
 
-    :: Remove special characters (just in case)
-    set "artist_name=!artist_name: =_!"
+    :: Extract artist name from URL (last part of URL)
+    for %%B in (!url!) do set "artistName=%%~nxB"
 
-    :: Define folder path
-    set "folder_path=%CD%\!artist_name!"
+    set "artistFolder=%downloadPath%\!artistName!"
 
-    :: Create the artist's folder if it doesn't exist
-    if not exist "!folder_path!" mkdir "!folder_path!"
+    echo.
+    echo Downloading tracks for !artistName!...
 
-    echo Downloading tracks for !artist_name! into "!folder_path!"...
+    :: Create artist folder
+    if not exist "!artistFolder!" mkdir "!artistFolder!"
 
-    :: Run scdl with the correct parameters
-    scdl -l "!url!" -t -c --path "!folder_path!" 
+    :: Fetch the total number of tracks (silent API request)
+    for /f "delims=" %%T in ('scdl -l "!url!" --show --no-download 2^>nul ^| find "Number of tracks:"') do (
+        for /f "tokens=4" %%N in ("%%T") do set "totalTracks=%%N"
+    )
 
-    echo Finished downloading !artist_name!
+    if not defined totalTracks set "totalTracks=Unknown"
+
+    echo Total tracks to download: !totalTracks!
+
+    set /a count=0
+    :: Use a loop to download each track individually
+    for /f "delims=" %%T in ('scdl -l "!url!" -t --path "!artistFolder!" --hide-progress 2^>nul') do (
+        set /a count+=1
+        call :show_progress !count! !totalTracks!
+    )
+
+    echo.
+    echo Fetching profile data for !artistName!...
+
+    :: Get metadata JSON
+    youtube-dl --write-info-json --skip-download "!url!" -o "!artistFolder!\metadata" >nul 2>&1
+
+    :: Extract profile picture
+    for /f "delims=" %%i in ('powershell -Command "(Get-Content '!artistFolder!\metadata.info.json' | ConvertFrom-Json).thumbnail"') do set "profileImage=%%i"
+    if defined profileImage (
+        echo Downloading profile picture...
+        curl -s "!profileImage!" -o "!artistFolder!\profile.jpg"
+    )
+
+    :: Extract header image
+    for /f "delims=" %%i in ('powershell -Command "(Get-Content '!artistFolder!\metadata.info.json' | ConvertFrom-Json).background_image_url"') do set "headerImage=%%i"
+    if defined headerImage (
+        echo Downloading header image...
+        curl -s "!headerImage!" -o "!artistFolder!\header.jpg"
+    )
+
+    :: Cleanup metadata JSON
+    del "!artistFolder!\metadata.info.json" 2>nul
+
+    echo.
+    echo Finished downloading for !artistName!
 )
 
-echo All downloads complete!
+echo.
+echo All downloads completed!
 pause
+exit /b
+
+:: Function to show progress bar
+:show_progress
+cls
+set /a percent=(%1*100)/%2
+set "bar="
+for /l %%i in (1,1,%percent%) do set "bar=!bar!#"
+echo [!bar!]
+echo Progress: %1 / %2 (%percent%%)
+exit /b
